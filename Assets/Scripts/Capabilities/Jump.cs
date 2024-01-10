4fc93802c772bbcbb648c425f5bd0619daf7d28b
Renan Sies <renansies@gmail.com>
2024-01-10 13:45:06 -0300

    Add coyote time in jump

--- Assets/Scripts/Capabilities/Jump.cs
+++ Assets/Scripts/Capabilities/Jump.cs
@@ -10,6 +10,7 @@ public class Jump : MonoBehaviour
     [SerializeField, Range(0, 5)] private int maxAirJumps = 0;
     [SerializeField, Range(0f, 5f)] private float downwardMovementMultiplier = 3f;
     [SerializeField, Range(0f, 5f)] private float upwardMovementMultiplier = 1.7f;
+    [SerializeField, Range(0f, 0.3f)] private float coyoteTime = 0.2f;
 
     private Rigidbody2D body;
     private Ground ground;
@@ -17,8 +18,11 @@ public class Jump : MonoBehaviour
 
     private int jumpPhase;
     private float defaultGravityScale;
+    private float jumpSpeed;
+    private float coyoteCounter;
     private bool desiredJump;
     private bool onGround;
+    private bool isJumping;
 
 
     // Start is called before the first frame update
@@ -35,9 +39,15 @@ public class Jump : MonoBehaviour
         onGround = ground.OnGround;
         velocity = body.velocity;
 
-        if (onGround)
+        if (onGround && body.velocity.y == 0)
         {
             jumpPhase = 0;
+            coyoteCounter = coyoteTime;
+            isJumping = false;
+        }
+        else
+        {
+            coyoteCounter -= Time.deltaTime;
         }
         if (desiredJump)
         {
@@ -68,10 +78,16 @@ public class Jump : MonoBehaviour
 
     private void JumpAction()
     {
-        if ( onGround || jumpPhase < maxAirJumps)
+        if ( coyoteCounter > 0f || (jumpPhase < maxAirJumps&& isJumping))
         {
-            jumpPhase += 1;
-            float jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight);
+            if (isJumping)
+            {
+                jumpPhase += 1;
+            }
+            coyoteCounter = 0;
+            jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * jumpHeight);
+            isJumping = true;
+
             if (velocity.y > 0f)
             {
                 jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
